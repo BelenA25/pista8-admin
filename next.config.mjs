8ef2a43e97f9178833c77bbb02901d6fc323463e
Belencita <beckiar23@gmail.com>
2024-08-30 12:54:19 -0400

    Fixing deploy issues

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,6 @@ const nextConfig = {
             "i.ibb.co"
         ]
     },
-    experimental:{
-        fontLoaders:[
-            {loader:'@next/font/google', options:{subsets:['latin']}}
-        ],
-    },
 };
 
 export default nextConfig;
