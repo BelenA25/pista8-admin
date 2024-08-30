cc8c81ae3e50dea72ef969f17d804fff402a727f
Belencita <beckiar23@gmail.com>
2024-08-30 12:09:25 -0400

    The title component was created

--- src/components/pagination-section.tsx
+++ src/components/pagination-section.tsx
@@ -18,7 +18,7 @@ export default function PaginationSection({ currentPage, totalPages, onPageChang
             <PaginationContent>
                 {currentPage > 1 && (
                     <PaginationItem>
-                        <PaginationPrevious href="#" onClick={(event) => { event.preventDefault(); handlePageChange(currentPage - 1); }}/>
+                        <PaginationPrevious href="#" onClick={(event) => { event.preventDefault(); handlePageChange(currentPage - 1); }} />
                     </PaginationItem>
                 )}
                 {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
@@ -30,7 +30,7 @@ export default function PaginationSection({ currentPage, totalPages, onPageChang
                 ))}
                 {currentPage < totalPages && (
                     <PaginationItem>
-                        <PaginationNext href="#" onClick={(event) => { event.preventDefault(); handlePageChange(currentPage + 1);}}/>
+                        <PaginationNext href="#" onClick={(event) => { event.preventDefault(); handlePageChange(currentPage + 1); }} />
                     </PaginationItem>
                 )}
             </PaginationContent>
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,7 +1,7 @@
 import AddButton from "./add-button";
 import Typography from "./Typography/typography";
 
-export default function Tittle() {
+export default function Title() {
     return (
         <div className="flex items-center justify-between p-4">
             <div className="flex-grow">
