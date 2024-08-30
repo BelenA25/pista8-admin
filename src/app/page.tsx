fed62fc36b7b025d068904ea859d65f3a6177e14
Belencita <beckiar23@gmail.com>
2024-08-30 12:20:27 -0400

    The principal page was improved by using dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+"use client"
+
+import dynamic from "next/dynamic";
+
+const StartupsPage = dynamic(() => import('@/app/startups/page'), { ssr: false });
 
 export default function Home() {
   return (
-    <></>
+    <div className="flex h-screen">
+      <div className="flex-grow p-4 overflow-auto  transition-all duration-300">
+      <StartupsPage />
+      </div>
+    </div>
   );
 }
