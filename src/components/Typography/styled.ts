3383d4ec51b729b587f5841d0b2d55b0f8eb34b0
Belencita <beckiar23@gmail.com>
2024-08-30 12:04:40 -0400

    The typography component was added

---
+++ src/components/Typography/styled.ts
@@ -0,0 +1,66 @@
+import { createElement, ReactNode, ElementType } from 'react';
+import styled, { css } from 'styled-components';
+
+interface DynamicTypographyProps {
+  tag: ElementType;
+  children: ReactNode;
+  [key: string]: any;
+}
+
+export const DynamicTypography = styled(({ tag, children, ...props }: DynamicTypographyProps) =>
+  createElement(tag, props, children),
+)`
+  ${({ tag }) => {
+    switch (tag) {
+      case 'h1':
+        return css`
+          font-size: 2.2rem;
+          font-weight: bold;
+          color: #FE420A;
+        `;
+      case 'h2':
+        return css`
+          font-size: 2rem;
+          font-weight: bold;
+        `;
+      case 'h3':
+        return css`
+          font-size: 1.75rem;
+          font-weight: bold;
+        `;
+      case 'h4':
+        return css`
+          font-size: 1.5rem;
+          font-weight: bold;
+        `;
+      case 'h5':
+        return css`
+          font-size: 1.25rem;
+          font-weight: bold;
+        `;
+      case 'h6':
+        return css`
+          font-size: 1rem;
+          font-weight: bold;
+        `;
+      case 'p':
+        return css`
+          font-size: 1rem;
+          font-weight: normal;
+          font-family: 'Lato', sans-serif;
+        `;
+      case 'span':
+        return css`
+          font-size: 0.875rem;
+          font-weight: normal;
+          font-family: 'Lato', sans-serif;
+        `;
+      default:
+        return css`
+          font-size: 1rem;
+          font-weight: normal;
+          font-family: 'Lato', sans-serif;
+        `;
+    }
+  }}
+`;
---
+++ src/components/Typography/typography.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ReactNode, CSSProperties } from 'react';
+import * as S from './styled';
+
+type TagVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
+
+interface TypographyProps {
+    tag?: TagVariants;
+    children: ReactNode;
+    alignment?: CSSProperties['textAlign'];
+    fontWeight?: "normal" | "semibold" | "bold";
+    noWrap?: boolean;
+}
+
+const Typography = ({
+    tag = 'p',
+    children,
+    alignment = 'center',
+    fontWeight = 'normal',
+    noWrap = false,
+}: TypographyProps) => {
+    const dynamicStyles = {
+        textAlign: alignment,
+        fontWeight: fontWeight === 'semibold' ? 600 : fontWeight === 'bold' ? 700 : 400,
+        marginLeft: alignment === 'center' ? 'auto' : undefined,
+        marginRight: alignment === 'center' ? 'auto' : undefined
+    };
+
+    return (
+        <S.DynamicTypography tag={tag} style={dynamicStyles} className={`max-w-screen-lg ${noWrap ? 'whitespace-nowrap' : ''}`}>
+            {children}
+        </S.DynamicTypography>
+    );
+};
+
+export default Typography;
