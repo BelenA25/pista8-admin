747fae2e1a23f3b52586500400907b249578a063
Belencita <beckiar23@gmail.com>
2024-08-30 12:19:28 -0400

    The startups page was improved by fetching data

--- src/app/startups/page.tsx
+++ src/app/startups/page.tsx
@@ -4,14 +4,38 @@ import PaginationSection from "@/components/pagination-section";
 import SearchCard from "@/components/search-card";
 import TableSection from "@/components/table-section";
 import Title from "@/components/title";
+import { useEffect, useState } from "react";
+import { database } from "../firebaseConfig";
+import { onValue, ref } from "firebase/database";
+
+const ITEMS_PAGE = 8;
 
 export default function Startups() {
-    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const startupsRef = ref(database, 'startups');
+        onValue(startupsRef, (snapshot) => {
+            const data = snapshot.val();
+            const startupList = data ? Object.values(data) : [];
+            setData(startupList);
+        });
+    }, []);
+
+    const totalPages = Math.ceil(data.length / ITEMS_PAGE);
+    const paginatedData = data.slice((currentPage - 1) * ITEMS_PAGE, currentPage * ITEMS_PAGE);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+
     return (
         <>
             <Title></Title>
             <SearchCard></SearchCard>
-            <TableSection data={}></TableSection>
-            <PaginationSection currentPage={} totalPages={} onPageChange={}></PaginationSection>        </>
+            <TableSection data={paginatedData}></TableSection>
+            <PaginationSection currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}></PaginationSection>        </>
     )
 }
