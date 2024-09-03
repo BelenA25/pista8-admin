1b7900c103709d22cfd52af677367a6f7a47bc69
Belencita <beckiar23@gmail.com>
2024-09-03 16:48:48 -0400

    The card search component was improved to have functionality

--- src/components/search-card.tsx
+++ src/components/search-card.tsx
@@ -3,12 +3,12 @@ import Typography from './Typography/typography';
 import { Card } from './ui/card';
 import SearchButton from './search-button';
 
-export default function SearchCard() {
+export default function SearchCard({ onClick }: { onClick: () => void }) {
     return (
         <Card className="mx-8 p-4 flex items-center space-x-5 border-none">
             <Typography tag='h6' noWrap={true}>Buscar Startup: </Typography>
             <Input type="text" placeholder="Buscar..." className="flex-grow p-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-green-500" />
-            <SearchButton></SearchButton>
+            <SearchButton onClick={onClick}></SearchButton>
         </Card>
     );
 }
