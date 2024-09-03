4f475ef6f91a5f57920ed005b28845bbadae4ec9
Belencita <beckiar23@gmail.com>
2024-09-03 16:42:44 -0400

    The search button component was improved to have functionality

--- src/components/search-button.tsx
+++ src/components/search-button.tsx
@@ -2,9 +2,9 @@ import { Button } from "./ui/button";
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 
 
-export default function SearchButton() {
+export default function SearchButton({ onClick }: { onClick: () => void }) {
     return (
-        <Button className="bg-white text-black rounded-r-lg flex items-center hover:bg-white hover:text-black">
+        <Button onClick={onClick} className="bg-white text-black rounded-r-lg flex items-center hover:bg-white hover:text-black">
             <MagnifyingGlassIcon className="w-6 h-6" />
         </Button>
     )
