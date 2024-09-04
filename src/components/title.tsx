ce32d88914577a153ac42a179ed2199d91e22841
Belencita <beckiar23@gmail.com>
2024-09-04 14:29:46 -0400

    The title component was improved

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,12 +1,16 @@
 import AddButton from "./add-button";
 import Typography from "./Typography/typography";
 
-export default function Title() {
+interface TitleProps {
+    text: string;
+}
+
+export default function Title({ text }: TitleProps) {
     return (
         <div className="flex items-center justify-between p-4">
             <div className="flex-grow">
                 <div className="text-2xl">
-                    <Typography tag="h1">Lista de Startups</Typography>
+                    <Typography tag="h1">{text}</Typography>
                 </div>
             </div>
             <AddButton></AddButton>
