a00bf7ab6bc5749a5fbc5e10cc63dfe6ba70594d
Belencita <beckiar23@gmail.com>
2024-09-09 20:48:05 -0400

    The component table row data was improved to be reusable

---
+++ src/app/mentors/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import PaginationSection from "@/components/pagination-section";
+import SearchCard from "@/components/search-card";
+import TableSection from "@/components/table-section";
+import Title from "@/components/title";
+import { useCallback, useEffect, useState } from "react";
+import { estimateTotalItems, fetchAllKeys, fetchData, handleResize } from "@/lib/utils";
+import LinkedInButton from "@/components/linkedin-button";
+
+const TYPE = 'mentors'
+
+export default function Mentors() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [data, setData] = useState<any[]>([]);
+    const [itemsPerPage, setItemsPerPage] = useState(6);
+    const [totalItems, setTotalItems] = useState<number>(0);
+    const [lastKeys, setLastKeys] = useState<string[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [allKeys, setAllKeys] = useState<string[]>([]);
+
+    useEffect(() => {
+        const onResize = () => handleResize(setItemsPerPage);
+
+        window.addEventListener('resize', onResize);
+        onResize();
+
+        return () => {
+            window.removeEventListener('resize', onResize);
+        };
+    }, []);
+
+    const fetchDataCallback = useCallback(() => {
+        fetchData(TYPE, searchTerm, currentPage, itemsPerPage, allKeys, setData, setLastKeys);
+    }, [searchTerm, currentPage, itemsPerPage, allKeys]);
+
+    useEffect(() => {
+        fetchAllKeys(TYPE, setAllKeys, setTotalItems);
+    }, []);
+    useEffect(() => {
+        estimateTotalItems(TYPE, setTotalItems);
+        fetchDataCallback();
+    }, [currentPage, itemsPerPage, searchTerm, fetchDataCallback]);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const handleSearchClick = (term: string) => {
+        setSearchTerm(term);
+    };
+
+    const handleDelete = () => {
+        fetchDataCallback();
+    };
+
+    const mapMentorsToRowDataProps = (item: any) => ({
+        itemId: item.id,
+        itemName: item.name,
+        imageUrl: item.imageUrl,
+        itemGeneric1: item.title,
+        itemGeneric2: item.city,
+        LinkedInButton: item.linkedin_link ? (
+            <LinkedInButton link={item.linkedin_link} />
+        ) : undefined
+    });
+
+    return (
+        <>
+            <Title text={"Lista de Mentores"} typeName={TYPE} ></Title>
+            <SearchCard onSearchClick={handleSearchClick} ></SearchCard>
+            <TableSection data={data} searchTerm={searchTerm} handleDelete={handleDelete} itemType={"mentors"} mapItemToRowDataProps={mapMentorsToRowDataProps}></TableSection>
+            {!searchTerm && (<PaginationSection currentPage={currentPage} totalPages={Math.ceil(totalItems / itemsPerPage)} onPageChange={handlePageChange}></PaginationSection>)}
+        </>
+    )
+}
--- src/components/table-row-data.tsx
+++ src/components/table-row-data.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import DeleteButton from "./delete-button";
 import EditButton from "./edit-button";
 import Typography from "./Typography/typography";
@@ -8,18 +9,20 @@ interface TableRowDataProps {
     itemId: string;
     itemName: string;
     imageUrl: string;
-    itemSector: string;
-    itemType: string; 
+    itemGeneric1: string;
+    itemGeneric2: string;
+    itemType: string;
+    LinkedInButton?: ReactNode;
     handleDelete: () => void;
-  }
+}
 
-export default function TableRowData({itemId, itemName, imageUrl, itemSector, itemType, handleDelete }: TableRowDataProps) {
+export default function TableRowData({ itemId, itemName, imageUrl, itemGeneric1, itemGeneric2, itemType, handleDelete, LinkedInButton }: TableRowDataProps) {
     return (
         <div className="border border-black rounded-lg">
             <TableRow className="w-full flex  border border-gray-400">
                 <TableCell className="w-1/18 flex items-center">
                     <Avatar>
-                        <AvatarImage src={imageUrl}/>
+                        <AvatarImage src={imageUrl} />
                         <AvatarFallback>{itemName.charAt(0)}</AvatarFallback>
                     </Avatar>
                 </TableCell>
@@ -28,14 +31,17 @@ export default function TableRowData({itemId, itemName, imageUrl, itemSector, it
                         {itemName}
                     </Typography>
                     <Typography tag="p" alignment="left">
-                        {itemSector}
+                        {itemGeneric1}{itemGeneric2 && ` - ${itemGeneric2}`}
                     </Typography>
                 </TableCell>
                 <TableCell className="flex justify-end items-center">
-                    <EditButton itemId={itemId} itemType={itemType}/>
+                    {LinkedInButton && LinkedInButton}
                 </TableCell>
                 <TableCell className="flex justify-end items-center">
-                    <DeleteButton itemId={itemId} onDelete={handleDelete} itemType={itemType}/>
+                    <EditButton itemId={itemId} itemType={itemType} />
+                </TableCell>
+                <TableCell className="flex justify-end items-center">
+                    <DeleteButton itemId={itemId} onDelete={handleDelete} itemType={itemType} />
                 </TableCell>
             </TableRow>
         </div>
