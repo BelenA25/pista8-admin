3e3031055cd849a56a17f61647b8b6f1bf97cbe7
CristhianRF24 <201803525@est.umss.edu>
2024-09-11 15:05:44 -0400

    I corrected the observations

--- src/components/table-section.tsx
+++ src/components/table-section.tsx
@@ -1,44 +1,60 @@
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import TableRowData from "./table-row-data";
-import TableRowPartners from './table-row-partners';
+import TableRowPartners from "./table-row-partners";
 
 interface TableRowDataProps {
-    name: string;
-    imageUrl: string;
-    sector: string;
+  name: string;
+  imageUrl: string;
+  sector: string;
 }
 
 interface TableRowPartnersProps {
-    id: string;
-    name: string;
-    imageUrl: string;
-    link: string;
-    itemType: string;
-    handleDelete: () => void;
+  id: string;
+  name: string;
+  imageUrl: string;
+  link: string;
+  itemType: string;
+  handleDelete: () => void;
 }
 interface TableSectionProps {
-    data?: TableRowDataProps[];
-    part?:TableRowPartnersProps[];
-    itemType: string;
-    handleDelete: () => void;
+  data?: TableRowDataProps[];
+  part?: TableRowPartnersProps[];
+  itemType: string;
+  handleDelete: () => void;
 }
 
-export default function TableSection({ data , part, itemType, handleDelete}: TableSectionProps) {
-       
-    return (
-        <div className="mb-4 ml-8 mr-8 mt-5 p-4 border border-black rounded-lg shadow-[0px_5px_5px_rgba(0,0,0,0.5)] overflow-hidden">
-            <Table className="w-full">
-               
-                <TableBody>
-                    {data?.map((item, index) => (
-                        <TableRowData key={index} name={item.name} imageUrl={item.imageUrl} sector={item.sector} />
-                    ))}
+export default function TableSection({
+  data,
+  part,
+  itemType,
+  handleDelete,
+}: TableSectionProps) {
+  return (
+    <div className="mb-4 ml-8 mr-8 mt-5 p-4 border border-black rounded-lg shadow-[0px_5px_5px_rgba(0,0,0,0.5)] overflow-hidden">
+      <Table className="w-full">
+        <TableBody>
+          {data?.map((item, index) => (
+            <TableRowData
+              key={index}
+              name={item.name}
+              imageUrl={item.imageUrl}
+              sector={item.sector}
+            />
+          ))}
 
-                    {part?.map((item, index)=>(
-                        <TableRowPartners key={index} itemName={item.name} imageUrl={item.imageUrl} link={item.link} itemType={itemType} itemId={item.id} handleDelete={handleDelete}/>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
-    );
+          {part?.map((item, index) => (
+            <TableRowPartners
+              key={index}
+              itemName={item.name}
+              imageUrl={item.imageUrl}
+              link={item.link}
+              itemType={itemType}
+              itemId={item.id}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
