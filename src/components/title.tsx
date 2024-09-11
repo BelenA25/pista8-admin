391201e5aada9eeb7d9d2fe8463b0900852fce5f
CristhianRF24 <201803525@est.umss.edu>
2024-09-11 18:41:32 -0400

    first commit

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,23 +1,19 @@
-import Link from "next/link";
 import AddButton from "./add-button";
 import Typography from "./Typography/typography";
 
-interface TitleProps {
-    title: string;
-    href: string;
-}
 
-export default function Title({title, href}:TitleProps) {
+
+export default function Title() {
     return (
         <div className="flex items-center justify-between p-4">
             <div className="flex-grow">
                 <div className="text-2xl">
-                    <Typography tag="h1">{title}</Typography>
+                    <Typography tag="h1">startupn name</Typography>
                 </div>
             </div>
-            <Link href={href}>
+         
             <AddButton/>
-            </Link>
+            
         </div>
     )
 }
